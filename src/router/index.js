24d728ad42a424f6a7a75098b950457c6a08c059
DreamBoat <3036265018@qq.com>
2024-11-24 03:31:49 +0800

    准备基础路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,10 @@
 // createWebHistory：（函数）创建 history 模式的路由 （常用的路由模式：history / hash）
 
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue'; // 导入一个路由级别的组件
+
+// 导入路由级别的组件
+import BigScreen from '../views/BigScreen.vue';
+import HomeView from '../views/HomeView.vue';
 
 // {}内的东西为 路由的配置对象，返回一个当前的路由实例，使用 router 接收
 const router = createRouter({
@@ -15,11 +18,19 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView, // （不管用不用都会加载资源）
+      redirect: '/big-screen' // 重定向
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/HomeView.vue'), // 路由的懒加载（只有访问这个路由的时候才会动态加载当前组件的所有相关资源）
     },
+    // 将来做微前端接入的时候，需要匹配一个和主应用中所有路由都不一样的独一无二的路由路径
+    // 所以定义了新的
+    {
+      path: '/big-screen',
+      name: 'bigScreen',
+      component: BigScreen
+    }
   ],
 })
 // 默认导出 router，导出以后在 main.js 中导入
---
+++ src/views/BigScreen.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>我是大屏显示组件</div>
+</template>
